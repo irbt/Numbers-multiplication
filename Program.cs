24cb8578d20080f47e8e5bfcd7253f9c161a786a
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-30 11:23:32 +0100

    final

--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@ namespace Program{
     class NumberMultiplicationTask{
         public static void Main(string [] args){
             Console.Clear();
-            static long NumbersMultiplication(int num , int exp){
+            static long NumbersMultiplication(int num1 , int num2){
                 
-                 return ;
+                 return  num1 > num2 ? 1 : num1 * NumbersMultiplication(num1+1, num2);
             }
             Console.WriteLine(NumbersMultiplication(5, 7));  //→ 210
             Console.WriteLine(NumbersMultiplication(50, 50)); // → 50
