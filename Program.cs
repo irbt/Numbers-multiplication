18fa69e8a135f2b9fcf9253d5528aa862d3c3936
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-30 11:09:30 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Given two integers a and b (a <= b) as range, write a method that returns multiplication of numbers from given range.
+
+
+namespace Program{
+    class NumberMultiplicationTask{
+        public static void Main(string [] args){
+            Console.Clear();
+            static long NumbersMultiplication(int num , int exp){
+                
+                 return ;
+            }
+            Console.WriteLine(NumbersMultiplication(5, 7));  //→ 210
+            Console.WriteLine(NumbersMultiplication(50, 50)); // → 50
+        }
+    }
+}
